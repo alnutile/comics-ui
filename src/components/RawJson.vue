3a91f66effcd59f9da06b0f6d70e71afcdfb38c7
Alfred Nutile <alfrednutile@gmail.com>
2021-03-12 19:29:34 -0500

    setting up vuex

---
+++ src/components/RawJson.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-card>
+    <vue-json-pretty :path="'res'" :data=json> </vue-json-pretty>
+  </b-card>
+</template>
+
+<script>
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
+export default {
+  components: {
+    VueJsonPretty,
+  },
+  props: ['json'],
+}
+</script>
+
